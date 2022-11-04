8601493c24be4d8c11d8851a0476f78e9739d1cd
Scott Rinaberger <scottrinaberger@gmail.com>
2022-11-04 16:55:57 -0500

    push tutor rollbacks

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,103 +1,101 @@
 const router = require('express').Router();
-const { Blog, User, } = require('../../models');
+const { Blog, User } = require("../../models");
 const withAuth = require("../../utils/auth");
+
 
 //Get and post routes
 // Get all blogs
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Blog.findAll()
-    .then(dbBlogData => res.json(dbBlogData))
-    .catch(err => {
+    .then((dbBlogData) => res.json(dbBlogData))     
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // Get a single blog
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Blog.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    attributes: [
-      'title',
-      'body',      
-      'user_id'
-    ],
+    attributes: ["title", "body", "user_id"],
     include: [
-    {
-      model: User,
-      attributes: ['username']
-    }
-  ]
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbBlogData => {
+    .then((dbBlogData) => {
       if (!dbBlogData) {
-        res.status(404).json({ message: 'No blog found with this id' });
+        res.status(404).json({ message: "No blog found with this id" });
         return;
       }
       res.json(dbBlogData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-    })
+    });
 });
 
 // Create a recipe
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   Blog.create({
     title: req.body.title,
-    body: req.body.body,    
+    body: req.body.body,
     user_id: req.session.user_id,
-
-  }).then(dbBlogData => res.json(dbBlogData))
-    .catch(err => {
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-    })
-})
+    });
+});
 
 // Update a blog
-router.put('/:id', (req, res) => {
-   // pass in req.body instead to only update what's passed through
+router.put("/:id", (req, res) => {
+  // pass in req.body instead to only update what's passed through
   Blog.update(req.body, {
     individualHooks: true,
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbBlogData => {
+    .then((dbBlogData) => {
       if (!dbBlogData) {
-        res.status(404).json({ message: 'No blog found with this id' });
+        res.status(404).json({ message: "No blog found with this id" });
         return;
       }
       res.json(dbBlogData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // Delete a blog
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Blog.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-  .then(dbBlogData => {
-    if (!dbBlogData) {
-      res.status(404).json({ message: 'No blog found with this id' });
-      return;
-    }
-    res.json(dbBlogData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-})
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-module.exports  = router;
+
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
 DROP DATABASE IF EXISTS cmsblogDB;
 
 CREATE DATABASE cmsblogDB;
+
